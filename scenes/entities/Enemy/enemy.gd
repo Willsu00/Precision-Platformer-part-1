aa701257f50cc7541e39296ab0596170e0f93663
William Sukjaem <133365639+Willsu00@users.noreply.github.com>
2024-03-14 21:19:40 +1300

    path finder

--- scenes/entities/Enemy/enemy.gd
+++ scenes/entities/Enemy/enemy.gd
@@ -1,34 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 150
-@export var nav_agent: NavigationAgent2D
-
-var target_node = null
-var home_pos = Vector2.ZERO
+@export var acceleration = 150
+@export var max_speed = 40
+@export var player_path : NodePath
 
 
-func _ready_():
-	home_pos = self.global_position
-	
-	nav_agent.path_desired_distance = 4
-	nav_agent.target_desired_distance = 4
-	
 func _physics_process(delta):
-	if nav_agent.is_navigation_finished():
-		return
-		
-	var axis = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = axis * speed
-	
-	
-	move_and_slide
-	
-func recalc_path():
-	if target_node:
-		nav_agent.target_position = target_node.global_position
-	else:
-		nav_agent.target_position = home_pos
+	if player_path is NodePath:
+		var player = get_node(player_path)
+		if player is CharacterBody2D:
+			move_toward_position(player.global_position,delta)
 			
+func move_toward_position(target_position,delta):
+	var direction = global_position.direction_to(target_position)
+	velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	move_and_slide()
+	
 	
 
 
